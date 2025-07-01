2504566b48e4e418307a513d17ee4f6810c56a6a
IhorSavva <savva.ihor@gmail.com>
2025-07-01 20:30:56 +0200

    Update versions

--- lib/src/jigsaw.dart
+++ lib/src/jigsaw.dart
@@ -99,7 +99,7 @@ class JigsawWidgetState extends State<JigsawWidget> {
   List<List<BlockClass>> images = <List<BlockClass>>[];
   ValueNotifier<List<BlockClass>> blocksNotifier =
       ValueNotifier<List<BlockClass>>(<BlockClass>[]);
-  CarouselController? _carouselController;
+  CarouselSliderController? _carouselController;
 
   Offset _pos = Offset.zero;
   int? _index;
@@ -116,7 +116,7 @@ class JigsawWidgetState extends State<JigsawWidget> {
     if (pngBytes == null) {
       throw InvalidImageException();
     }
-    return ui.decodeImage(List<int>.from(pngBytes));
+    return ui.decodeImage(pngBytes);
   }
 
   void reset() {
@@ -181,10 +181,10 @@ class JigsawWidgetState extends State<JigsawWidget> {
 
         final ui.Image temp = ui.copyCrop(
           fullImage!,
-          xAxis.round(),
-          yAxis.round(),
-          widthPerBlockTemp.round(),
-          heightPerBlockTemp.round(),
+          x: xAxis.round(),
+          y: yAxis.round(),
+          width: widthPerBlockTemp.round(),
+          height: heightPerBlockTemp.round(),
         );
 
         final Offset offset = Offset(size!.width / 2 - widthPerBlockTemp / 2,
@@ -222,7 +222,7 @@ class JigsawWidgetState extends State<JigsawWidget> {
 
   @override
   void initState() {
-    _carouselController = CarouselController();
+    _carouselController = CarouselSliderController();
     super.initState();
   }
 
